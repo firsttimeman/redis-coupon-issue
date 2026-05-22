e0868636f40b165d2e74f4df21d0142788946729
sungsanghwa <sangwha0@naver.com>
2026-05-23 00:42:22 +0900

    스프링 부트 기본 구조 추가

---
+++ src/main/java/com/example/coupon/CouponIssueApplication.java
@@ -0,0 +1,12 @@
+package com.example.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponIssueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponIssueApplication.class, args);
+    }
+}
